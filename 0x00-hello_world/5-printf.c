6864845a1fd189c34530dca66c68555651518444
Jamescog <jamescog72@gmail.com>
2022-04-07 11:11:09 -0700

    initial commit

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,15 +1,28 @@
 #include <stdio.h>
-/*
- * this program prints "with proper grammer, but the outcome is a piece of art,"
- * followed by a new line
- * 
- * Return :always 0.
- * by Yaekob Demisse
+
+/**
+ * main - Prints "with proper grammar, but the outcome is
+ *                a piece of art," followed by a new line.
+ *
+ * Return: Always 0.
  */
 int main(void)
 {
-	printf("with proper grammer, but the outcome is a piece of art,\n");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 
 	return (0);
-
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
