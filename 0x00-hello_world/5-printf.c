babc504027cb49b16dc838b6e5b68b1d0ea60d9c
Jamescog <jamescog72@gmail.com>
2022-04-07 11:07:00 -0700

    initial commit

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/*
+ * this program prints "with proper grammer, but the outcome is a piece of art,"
+ * followed by a new line
+ * 
+ * Return :always 0.
+ * by Yaekob Demisse
+ */
+int main(void)
+{
+	printf("with proper grammer, but the outcome is a piece of art,\n");
+
+	return (0);
+
+}
