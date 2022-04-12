e6499860c4c0370a936a9fce98cc888bf07c5b1f
Jamescog <jamescog72@gmail.com>
2022-04-12 10:01:17 -0700

    initial commit

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_alphabet- check the code
+ *
+ * Return: always 0.
+ */
+void print_alphabet(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+		_putchar(letter);
+
+	_putchar('\n');
+}
