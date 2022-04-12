81f4fc363a23468da5b85f48b36bac4761089cc4
Jamescog <jamescog72@gmail.com>
2022-04-12 09:33:16 -0700

    initial commit

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * Main - Prints _putchar
+ *
+ * Return - always return 0.
+ */
+int main(void)
+{
+	_putchar('-');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,2 @@
+ This is are tasks on c functions and nested loops 
+each task is given by alx software engineering courses
