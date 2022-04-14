d76fd5c69fcbf536ec8566bea8e46a8b9bc3a17d
Jamescog <94002610+Jamescog@users.noreply.github.com>
2022-04-14 23:42:34 +0300

    Create 8-print_square.c

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ *print_square - print square of specified size
+ *@size: integer
+ */
+void print_square(int size)
+{
+	int x;
+	int y;
+
+	if (size > 0)
+	{
+		for (x = 0; x < size; x++)
+		{
+			for (y = 0; y < size; y++)
+			{
+				_putchar ('#');
+			}
+			_putchar ('\n');
+		}
+	}
+	else
+	{
+		_putchar ('\n');
+	}
+}
