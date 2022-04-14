5ad2b9b318668981a392ccf0f26c88c7f9586192
Jamescog <jamescog72@gmail.com>
2022-04-14 03:36:32 -0700

    code commented out

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -1,24 +1,254 @@
 #include <stdio.h>
-
 /**
-* main - causes an infinite loop
-* Return: 0
-*/
-
+ * main - causes an infinite loop
+ * Return: 0
+ */
 int main(void)
 {
-        int i;
+	/*
+	 * int i;
+	 */
+	printf("Infinite loop incoming :(\n");
+	/*
+	 * i = 0;
+	 */
+	/*
+	 * while (i < 10)
+	 * {
+	 * putchar(i);
+	 * }
+	 */
+	printf("Infinite loop avoided! \\o/\n");
 
-        printf("Infinite loop incoming :(\n");
+	return (0);
+}#include <stdio.h>
+/**
+ * main - causes an infinite loop
+ * Return: 0
+ */
+int main(void)
+{
+	/*
+	 * int i;
+	 */
+	printf("Infinite loop incoming :(\n");
+	/*
+	 * i = 0;
+	 */
+	/*
+	 * while (i < 10)
+	 * {
+	 * putchar(i);
+	 * }
+	 */
+	printf("Infinite loop avoided! \\o/\n");
 
-        i = 0;
+	return (0);
+}#include <stdio.h>
+/**
+ * main - causes an infinite loop
+ * Return: 0
+ */
+int main(void)
+{
+	/*
+	 * int i;
+	 */
+	printf("Infinite loop incoming :(\n");
+	/*
+	 * i = 0;
+	 */
+	/*
+	 * while (i < 10)
+	 * {
+	 * putchar(i);
+	 * }
+	 */
+	printf("Infinite loop avoided! \\o/\n");
 
-      /** while (i < 10)
-       *{
-       *        putchar(i);
-       *}
-       */
-        printf("Infinite loop avoided! \\o/\n");
+	return (0);
+}#include <stdio.h>
+/**
+ * main - causes an infinite loop
+ * Return: 0
+ */
+int main(void)
+{
+	/*
+	 * int i;
+	 */
+	printf("Infinite loop incoming :(\n");
+	/*
+	 * i = 0;
+	 */
+	/*
+	 * while (i < 10)
+	 * {
+	 * putchar(i);
+	 * }
+	 */
+	printf("Infinite loop avoided! \\o/\n");
 
-        return (0);
+	return (0);
+}#include <stdio.h>
+/**
+ * main - causes an infinite loop
+ * Return: 0
+ */
+int main(void)
+{
+	/*
+	 * int i;
+	 */
+	printf("Infinite loop incoming :(\n");
+	/*
+	 * i = 0;
+	 */
+	/*
+	 * while (i < 10)
+	 * {
+	 * putchar(i);
+	 * }
+	 */
+	printf("Infinite loop avoided! \\o/\n");
+
+	return (0);
+}#include <stdio.h>
+/**
+ * main - causes an infinite loop
+ * Return: 0
+ */
+int main(void)
+{
+	/*
+	 * int i;
+	 */
+	printf("Infinite loop incoming :(\n");
+	/*
+	 * i = 0;
+	 */
+	/*
+	 * while (i < 10)
+	 * {
+	 * putchar(i);
+	 * }
+	 */
+	printf("Infinite loop avoided! \\o/\n");
+
+	return (0);
+}#include <stdio.h>
+/**
+ * main - causes an infinite loop
+ * Return: 0
+ */
+int main(void)
+{
+	/*
+	 * int i;
+	 */
+	printf("Infinite loop incoming :(\n");
+	/*
+	 * i = 0;
+	 */
+	/*
+	 * while (i < 10)
+	 * {
+	 * putchar(i);
+	 * }
+	 */
+	printf("Infinite loop avoided! \\o/\n");
+
+	return (0);
+}#include <stdio.h>
+/**
+ * main - causes an infinite loop
+ * Return: 0
+ */
+int main(void)
+{
+	/*
+	 * int i;
+	 */
+	printf("Infinite loop incoming :(\n");
+	/*
+	 * i = 0;
+	 */
+	/*
+	 * while (i < 10)
+	 * {
+	 * putchar(i);
+	 * }
+	 */
+	printf("Infinite loop avoided! \\o/\n");
+
+	return (0);
+}#include <stdio.h>
+/**
+ * main - causes an infinite loop
+ * Return: 0
+ */
+int main(void)
+{
+	/*
+	 * int i;
+	 */
+	printf("Infinite loop incoming :(\n");
+	/*
+	 * i = 0;
+	 */
+	/*
+	 * while (i < 10)
+	 * {
+	 * putchar(i);
+	 * }
+	 */
+	printf("Infinite loop avoided! \\o/\n");
+
+	return (0);
+}#include <stdio.h>
+/**
+ * main - causes an infinite loop
+ * Return: 0
+ */
+int main(void)
+{
+	/*
+	 * int i;
+	 */
+	printf("Infinite loop incoming :(\n");
+	/*
+	 * i = 0;
+	 */
+	/*
+	 * while (i < 10)
+	 * {
+	 * putchar(i);
+	 * }
+	 */
+	printf("Infinite loop avoided! \\o/\n");
+
+	return (0);
+}#include <stdio.h>
+/**
+ * main - causes an infinite loop
+ * Return: 0
+ */
+int main(void)
+{
+	/*
+	 * int i;
+	 */
+	printf("Infinite loop incoming :(\n");
+	/*
+	 * i = 0;
+	 */
+	/*
+	 * while (i < 10)
+	 * {
+	 * putchar(i);
+	 * }
+	 */
+	printf("Infinite loop avoided! \\o/\n");
+
+	return (0);
 }
