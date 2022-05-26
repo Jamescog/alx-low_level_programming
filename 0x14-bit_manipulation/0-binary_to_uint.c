b607cc204e7d6ab228756cefd5861c193d58b507
Jamescog <jamescog72@gmail.com>
2022-05-26 13:40:23 -0700

    betty be humble

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -12,13 +12,13 @@ unsigned int binary_to_uint(const char *b)
     unsigned int dec_val = 0;
 
     if (!b)
-        return (0);
+	    return (0);
 
     for (i = 0; b[i]; i++)
     {
-        if (b[i] < '0' || b[i] > '1')
-            return (0);
-        dec_val = 2 * dec_val + (b[i] - '0');
+	    if (b[i] < '0' || b[i] > '1')
+		    return (0);
+	    dec_val = 2 * dec_val + (b[i] - '0');
     }
 
     return (dec_val);
