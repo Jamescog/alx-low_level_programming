386529b7e268cf3818b5be65a049811fcd433bb0
Jamescog <jamescog72@gmail.com>
2022-08-12 05:20:47 -0700

    24 theme

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -7,7 +7,7 @@ void jack_bauer(void)
 	int hour, minute;
 	for (hour = 0; hour <= 24; hour++)
 	{
-		for (minute = 0; minute = 59; minute++)
+		for (minute = 0; minute <= 59; minute++)
 		{
 			_putchar((hour / 10) + '0');
 			_putchar((hour % 10) + '0');
