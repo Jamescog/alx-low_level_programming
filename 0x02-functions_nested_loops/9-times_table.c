6d6e7a2aaa66808cd1176b1d042b5c93753eeeba
Jamescog <94002610+Jamescog@users.noreply.github.com>
2022-08-12 16:22:41 +0300

    9 times 9 table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -4,25 +4,37 @@
  */
 void times_table(void)
 {
-	int num, mult, prod;
+	int row, column, mul;
 
-	for (num = 0; num <= 9; num++)
+	for (column = 0; column <= 9; column++)
 	{
-		_putchar('0');
-
-		for (mult = 1; mult <= 9; mult++)
+		for (row = 0; row <= 9; row++)
 		{
-			_putchar(',');
-			_putchar(' ');
-
-			prod = num * mult;
-
-			if (prod <= 9)
-				_putchar(' ');
+			mul = row * column;
+			if (mul <= 9)
+			{
+				if (row == 9)
+				{
+					_putchar(' ');
+					_putchar((mul % 10) + '0');
+				}
+				else
+				{
+					_putchar((mul % 10) + '0');
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+				}
+			}
 			else
-				_putchar((prod / 10) + '0');
-
-			_putchar((prod % 10) + '0');
+			{
+				_putchar((mul / 10) + '0');
+				_putchar((mul % 10) + '0');
+				if (row == 9)
+					continue;
+				_putchar(',');
+				_putchar(' ');
+			}
 		}
 		_putchar('\n');
 	}
