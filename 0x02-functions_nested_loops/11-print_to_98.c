698cdf7626e58a114ec5cf7e044df4a4d2117783
Jamescog <94002610+Jamescog@users.noreply.github.com>
2022-08-13 12:20:19 +0300

    Update 11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -3,7 +3,7 @@
 /**
  * print_to_98 - Prints all natural numbers from input to 98,
  *               in order separated by a comma followed by a space.
- * pputchar_integer - take care the printing using _putchar
+ * pputchar_integer - Take care the printing using _putchar
  * @n: The number to begin counting at.
  */
 void pputchar_integer(int n)
@@ -24,7 +24,7 @@ void print_to_98(int n)
 {
 	if (n <= 98)
 	{
-		while (n <= 98)
+		while (n < 99)
 		{
 			pputchar_integer(n);
 			if (n == 98)
@@ -39,7 +39,7 @@ void print_to_98(int n)
 	}
 	else
 	{
-		while (n >= 98)
+		while (n > 97)
 		{
 			pputchar_integer(n);
 			if (n == 98)
