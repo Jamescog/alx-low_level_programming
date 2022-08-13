2a265210136445b17c90957134c276513c1e9b6a
Jamescog <94002610+Jamescog@users.noreply.github.com>
2022-08-13 12:12:55 +0300

    return type is fixed

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -12,7 +12,7 @@ int print_last_digit(int);
 void jack_bauer(void);
 void times_table(void);
 int add(int, int);
-int pputchar_integer(int n);
+void pputchar_integer(int n);
 void print_to_98(int n);
 void print_times_table(int n);
 
