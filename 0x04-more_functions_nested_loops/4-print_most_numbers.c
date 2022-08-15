f106550ee113cd2216869e5d3914a8e1c1643007
pythinker <Jamescog>
2022-08-15 15:02:33 +0300

    print but select

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -6,12 +6,15 @@
  */
 void print_most_numbers(void)
 {
-	int c;
+	int num;
 
-	for (c = 0; c <= 9; c++)
+	for (num = 0; num <= 9; num++)
 	{
-		if (c != 2 && c != 4)
-			_putchar (c + '0');
+		if (num == 2 || num == 4)
+		{
+			continue;
+		}
+		_putchar((num % 10) + '0');
 	}
-	_putchar ('\n');
+	_putchar('\n');
 }
