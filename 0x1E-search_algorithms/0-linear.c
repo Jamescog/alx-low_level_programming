904e4e64dd2d8bc2f18480a1ec01b59fe494ac2d
Jamescog <jamescog72@gmail.com>
2022-11-06 22:05:17 +0300

    linear search implementation

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -11,14 +11,17 @@ int linear_search(int *array, size_t size, int value)
 {
 	size_t i;
 
+	if (size == 0)
+	{
+		return (-1);
+	}
 	for (i = 0; i < size; i++)
 	{
 		printf("Value checked array[%ld] = [%d]\n", i, array[i]);
-		if (array[i] == value)
+		if (value == array[i])
 		{
 			return (i);
 		}
 	}
-
 	return (-1);
 }
